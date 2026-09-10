edca287d75c54a9730cc23789dcd184cad252b2b
dotJManuel <dotjmanuel@gmail.com>
2026-09-10 17:21:23 -0600

    chore: migrate from .net7 to .net10

--- TareasMVC/Program.cs
+++ TareasMVC/Program.cs
@@ -58,7 +58,7 @@ builder.Services.AddLocalization(opciones =>
 });
 
 builder.Services.AddTransient<IServicioUsuarios, ServicioUsuarios>();
-builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddAutoMapper(cfg => { }, typeof(Program));
 builder.Services.AddTransient<IAlmacenadorArchivos, AlmacenadorArchivosLocal>();
 
 var app = builder.Build();
